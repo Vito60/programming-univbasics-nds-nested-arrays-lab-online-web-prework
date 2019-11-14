c24dbde477611fa29ae285bd060d66b3e6fcfb09
Randall D Williams <randallw1996@gmail.com>
2019-11-14 22:22:22 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -45,12 +45,8 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
-  matrix_update = [
-    ENNET_HOUSE,
-    ENFIELD_TENNIS_ACADEMY
-    ]
-    matrix_update[0][2] = "Tiny Ewell"
-    matrix_update
+  matrix[row][column] = new_value
+  matrix
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
 end
